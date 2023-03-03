9a1fc35e1816a0e43b7ee73029b125faa897f510
Edgaras <edgaras.tkacenka@wallee.com>
2023-03-03 17:11:03 +0200

    Updated sdk version

--- wallee-sdk/autoload.php
+++ wallee-sdk/autoload.php
@@ -3,8 +3,6 @@
  * wallee SDK
  *
  * This library allows to interact with the wallee payment service.
- * wallee SDK: 1.0.0
- *
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -17,48 +15,47 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- * @author customweb GmbH (http://www.customweb.com/)
- * @copyright 2017 - 2023 customweb GmbH
- * @license http://www.apache.org/licenses/LICENSE-2.0 Apache Software License (ASL 2.0)
  */
+
 
 /**
  * Autoload function.
  *
+ * @author   customweb GmbH
+ 
  * After registering this autoload function with SPL, the following line
  * would cause the function to attempt to load the \Wallee\Sdk\Baz\Qux class
  * from /path/to/project/lib/Baz/Qux.php:
  *
- *      new \Wallee\Sdk\Baz\Qux;
+ *	  new \Wallee\Sdk\Baz\Qux;
  *
  * @param string $class the fully-qualified class name.
  */
 spl_autoload_register(function ($class) {
 
-    // project-specific namespace prefix
-    $prefix = 'Wallee\\Sdk\\';
+	// project-specific namespace prefix
+	$prefix = 'Wallee\\Sdk\\';
 
-    // base directory for the namespace prefix
-    $baseDir = dirname(__FILE__) . '/lib/';
+	// base directory for the namespace prefix
+	$baseDir = __DIR__ . '/lib/';
 
-    // does the class use the namespace prefix?
-    $len = Tools::strlen($prefix);
-    if (strncmp($prefix, $class, $len) !== 0) {
-        // no, move to the next registered autoloader
-        return;
-    }
+	// does the class use the namespace prefix?
+	$len = strlen($prefix);
+	if (strncmp($prefix, $class, $len) !== 0) {
+		// no, move to the next registered autoloader
+		return;
+	}
 
-    // get the relative class name
-    $relativeClass = Tools::substr($class, $len);
+	// get the relative class name
+	$relativeClass = substr($class, $len);
 
-    // replace the namespace prefix with the base directory, replace namespace
-    // separators with directory separators in the relative class name, append
-    // with .php
-    $file = $baseDir . str_replace('\\', '/', $relativeClass) . '.php';
+	// replace the namespace prefix with the base directory, replace namespace
+	// separators with directory separators in the relative class name, append
+	// with .php
+	$file = $baseDir . str_replace('\\', '/', $relativeClass) . '.php';
 
-    // if the file exists, require it
-    if (file_exists($file)) {
-        require $file;
-    }
+	// if the file exists, require it
+	if (file_exists($file)) {
+		require $file;
+	}
 }, true, true);
